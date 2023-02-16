d38092463d8969b23b48666ee74000a986948854
chillboi <liamhuangzzz@gmail.com>
2023-02-16 16:22:41 +0800

    chore: release v1.0.0

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    trailingComma: 'all',
+    printWidth: 120,
+    useTabs: false,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    jsxBracketSameLIne: false,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    plugin: [require.resolve('prettier-plugin-packagejson')],
+}
